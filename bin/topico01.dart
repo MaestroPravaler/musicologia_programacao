3097b2415b44fafa3d678b2a69713527de805ff0
RobsonMaestro <robsonmaestro@pop-os.localdomain>
2020-11-08 21:26:01 -0300

    Inicio do Projeto

---
+++ bin/topico01.dart
@@ -0,0 +1,5 @@
+import 'package:topico01/topico01.dart' as topico01;
+
+void main(List<String> arguments) {
+  print('Hello world: ${topico01.calculate()}!');
+}
---
+++ test/topico01_test.dart
@@ -0,0 +1,8 @@
+import 'package:topico01/topico01.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('vou multiplicar algo', () {
+    expect(calculate(), 42);
+  });
+}
